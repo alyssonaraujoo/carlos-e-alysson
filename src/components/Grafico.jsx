28d117bf25d106aba7606ce13f3f2ca2d95971e6
Carlos Roberto <carloos.bezerra@hotmail.com>
2025-02-02 15:22:48 -0300

    Adicionando biblioteca de gráficos rechards com dados mocados

--- src/components/Grafico.jsx
+++ src/components/Grafico.jsx
@@ -1,8 +1,35 @@
 import './Grafico.css';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+
 
 function Grafico() {
+
+  const data = [
+    { name: 'Energia', valor: 525, pv: 2400, amt: 2400 },
+    { name: 'Aluguel', valor: 1500, pv: 2400, amt: 2400 },
+    { name: 'Internet', valor: 99, pv: 2400, amt: 2400 },
+    { name: 'Plano de saúde', valor: 200, pv: 2400, amt: 2400 }
+  ];
+
+  const renderBarChart = (
+    <BarChart width={600} height={300} data={data}>
+      <XAxis dataKey="name" stroke="#8884d8" />
+      <YAxis />
+      <Tooltip />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <Bar dataKey="valor" fill="#8884d8" barSize={30} />
+    </BarChart>
+  );
   return (
-    <div>Gráfico</div>
+    <>
+      <h1>Gráfico</h1>
+      <div>{renderBarChart}</div>
+    </>
+
+
+
+
+
   )
 }
 
