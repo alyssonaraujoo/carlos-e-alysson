9a6a66013348652f2725956724a324a9d5606e6c
alysson <alyssonmusic@live.com>
2025-02-02 16:31:02 -0300

    Deixando os autores um do lado do outro nos arquivos 'Footer.jsx' e 'Footer.css'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,23 @@ import "./Footer.css";
 function Footer() {
     return (
         <footer>
-            <p>Autor: <br /> Carlos Roberto <br />
-                <p>Contato: <br />
-                    <a href="https://www.linkedin.com/in/carlos-roberto-alves-bezerra-luna-125288224/" target="blank">
-                        <img src="https://icons.iconarchive.com/icons/graphics-vibe/simple-rounded-social/48/linkedin-icon.png" alt="Linkedin" />
-                    </a>
+            <div className="autores">
+                <p>Autor: <br /> Carlos Roberto <br />
+                    <p>Contato: <br />
+                        <a href="https://www.linkedin.com/in/carlos-roberto-alves-bezerra-luna-125288224/" target="blank">
+                            <img src="https://icons.iconarchive.com/icons/graphics-vibe/simple-rounded-social/48/linkedin-icon.png" alt="Linkedin" />
+                        </a>
+                    </p>
                 </p>
-            </p>
-            <p>Autor: <br /> Alysson Araujo <br />
-                <p>Contato: <br />
-                    <a href="https://www.linkedin.com/in/alysson-araujo/
-" target="blank">
-                        <img src="https://icons.iconarchive.com/icons/graphics-vibe/simple-rounded-social/48/linkedin-icon.png" alt="Linkedin" />
-                    </a>
+                <p>Autor: <br /> Alysson Araujo <br />
+                    <p>Contato: <br />
+                        <a href="https://www.linkedin.com/in/alysson-araujo/
+    " target="blank">
+                            <img src="https://icons.iconarchive.com/icons/graphics-vibe/simple-rounded-social/48/linkedin-icon.png" alt="Linkedin" />
+                        </a>
+                    </p>
                 </p>
-            </p>
-
+            </div>
             <a href="#politica-de-privacidade">Política de Privacidade</a>
             <a href="#termos-de-uso">Termos de Uso</a>
             <a href="#ajuda">Ajuda</a>
